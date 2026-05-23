7ab664ff55ae9dd7db4d6132d9cf24d29d7f86eb
Leonardo <l3onav@example.com>
2026-05-22 17:52:59 -0700

    Fix tpm2-pytss and openldap test failures, remove deprecated podman.enableNvidia

--- declaration.nix
+++ declaration.nix
@@ -9,18 +9,18 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
-  
+
   programs.gamemode = {
-   enable = true;
-   settings = {
-    general = {
-      renice = 10;  # Give games higher priority
+    enable = true;
+    settings = {
+      general = {
+        renice = 10; # Give games higher priority
+      };
+      gpu = {
+        apply_gpu_optimisations = "accept_responsibility";
+        gpu_device = 0;
+      };
     };
-    gpu = {
-      apply_gpu_optimisations = "accept_responsibility";
-      gpu_device = 0;
-    };
-   };
   };
 
   services.openssh.enable = true;
@@ -30,7 +30,7 @@
       enable = true;
       dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
-      enableNvidia = true;
+      # enableNvidia is deprecated; hardware.nvidia-container-toolkit.enable is set in configuration.nix
     };
   };
   environment.etc."cdi/nvidia.yaml".source = "/run/cdi/nvidia-container-toolkit.json";
